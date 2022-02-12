da04351f853ddfdedf10d2eb561cdb879abb8922
BelenMaru <bleneee@gmail.com>
2022-02-11 22:58:42 -0500

    add moal to the sidebar

---
+++ sidebar-intro/src/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div className={`modal-overlay show-modal`}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+      </div>
+    </div>
+  );
+};
+export default Modal;
