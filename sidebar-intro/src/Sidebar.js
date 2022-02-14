ed8f23edd88e22d8fdba66142903dd3d6e71a2c3
BelenMaru <bleneee@gmail.com>
2022-02-14 18:55:22 -0500

    add data to the sidebar

--- sidebar-intro/src/Sidebar.js
+++ sidebar-intro/src/Sidebar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
-
+import { FaTimes } from "react-icons/fa";
 const Sidebar = () => {
   return (
     <aside className={`sidebar show-sidebar`}>
       <div className="sidebar-header">
         <img src={logo} className="logo" alt="coding addict" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
       </div>
     </aside>
   );
