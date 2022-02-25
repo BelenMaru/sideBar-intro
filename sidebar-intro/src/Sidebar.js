66fd1d709762669cc6510da08b35e53710378c5b
BelenMaru <bleneee@gmail.com>
2022-02-24 19:15:54 -0500

    import social and links from data

--- sidebar-intro/src/Sidebar.js
+++ sidebar-intro/src/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+
 const Sidebar = () => {
   return (
     <aside className={`sidebar show-sidebar`}>
