14718c714d689c94b250fb576a28eee819341871
BelenMaru <bleneee@gmail.com>
2022-05-06 18:38:11 -0400

    cleanup file

--- sidebar-intro/src/context.js
+++ sidebar-intro/src/context.js
@@ -2,10 +2,3 @@ import React,{useState,useContext} from 'react'
 
 const AppContext = React.createContext();
 export{AppContext}
-// function App() {
-//   return (
-//     <h2>Lists</h2>
-//   );
-// }
-
-// export default App;
