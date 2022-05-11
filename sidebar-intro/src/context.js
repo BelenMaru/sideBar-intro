9b93f60dc2531dd70285829c06abb89bcc2c4f00
BelenMaru <bleneee@gmail.com>
2022-05-10 20:52:43 -0400

    adding custom hook

--- sidebar-intro/src/context.js
+++ sidebar-intro/src/context.js
@@ -2,3 +2,9 @@ import React,{useState,useContext} from 'react'
 
 const AppContext = React.createContext();
 export{AppContext}
+
+// custom hook
+export const useGlobalContext =()=> {
+  return useContext(AppContext)
+}
+export { AppContext}; 
