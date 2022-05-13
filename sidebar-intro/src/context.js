afb4bb17b33690413996b1793ebf51b768c50c1e
BelenMaru <bleneee@gmail.com>
2022-05-12 22:40:19 -0400

    adding value to context

--- sidebar-intro/src/context.js
+++ sidebar-intro/src/context.js
@@ -2,6 +2,7 @@ import React,{useState,useContext} from 'react'
 
 const AppContext = React.createContext();
 const AppProvider = ({children}) => {
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 }
 // custom hook
 export const useGlobalContext =()=> {
