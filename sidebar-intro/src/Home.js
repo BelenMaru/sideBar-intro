503ae2af0b9db347e6f83d24a0a88b690b951499
BelenMaru <bleneee@gmail.com>
2022-06-21 19:30:13 -0400

    checking data at context

--- sidebar-intro/src/Home.js
+++ sidebar-intro/src/Home.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
 import { AppContext, useGlobalContext } from "./context";
 const Home = () => {
+  // const data = useContext(AppContext);
+  const data = useGlobalContext();
+  console.log(data);
   return (
     <main>
       <button className="sidebar-toggle">
@@ -10,7 +13,6 @@ const Home = () => {
       <button className="btn">show modal</button>
     </main>
   );
-  
 };
 
 export default Home;
