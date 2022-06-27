6c74a5b1e88e820384cd25c9f63360e560621fc2
BelenMaru <bleneee@gmail.com>
2022-06-27 17:51:54 -0400

    checking the context

--- sidebar-intro/src/App.js
+++ sidebar-intro/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
+import context from './context'
 function App() {
   return (
     <>
