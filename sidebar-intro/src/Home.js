f32a601bee22ce8dbc24464bf6bfea73aecac2da
BelenMaru <bleneee@gmail.com>
2022-07-16 22:05:36 -0400

    fixing home buttons

--- sidebar-intro/src/Home.js
+++ sidebar-intro/src/Home.js
@@ -1,18 +1,18 @@
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
 import { AppContext, useGlobalContext } from "./Context";
-// const Home = () => {
-//   // const data = useContext(AppContext);
-//   const data = useGlobalContext();
-//   console.log(data);
-//   return (
-//     <main>
-//       <button className="sidebar-toggle">
-//         <FaBars />
-//       </button>
-//       <button className="btn">show modal</button>
-//     </main>
-//   );
-// };
+const Home = () => {
+  // const data = useContext(AppContext);
+  const data = useGlobalContext();
+  console.log(data);
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn">show modal</button>
+    </main>
+  );
+};
 
 export default Home;
