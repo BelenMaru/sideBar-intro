41739b4d3091337f049e5c633262e3b89ded853e
BelenMaru <bleneee@gmail.com>
2022-08-24 10:41:05 -0400

    refactor the sidebar

--- sidebar-intro/src/Sidebar.js
+++ sidebar-intro/src/Sidebar.js
@@ -5,7 +5,7 @@ import { social, links } from "./data";
 import { useGlobalContext } from "./context";
 const Sidebar = () => {
   return (
-    <aside className={`sidebar show-sidebar`}>
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
       <div className="sidebar-header">
         <img src={logo} className="logo" alt="coding addict" />
         <button className="close-btn">
